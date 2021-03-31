f39c1ddd91aeb7e6ecdf29dbf4bf8b8e8b9bbb82
Rohan <rohans540@gmail.com>
2021-03-31 18:10:21 +0530

    dom manipulation using js

---
+++ FormValidation/script.js
@@ -0,0 +1,48 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email')
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+
+function showSuccess(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+function isValidEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    
+    if(username.value === '') {
+        showError(username, 'Username is required');
+    } else {
+        showSuccess(username);
+    }
+
+    if(email.value === '') {
+        showError(email, 'email is required');
+    } else if(!isValidEmail(email)) {
+        showError(email, 'Invalid format for Email');
+    } 
+    else {
+        showSuccess(email);
+    }
+
+    if(password.value === '') {
+        showError(password, 'password is required');
+    } else {
+        showSuccess(password);
+    }
+})
