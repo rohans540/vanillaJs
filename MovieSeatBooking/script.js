970134f0b08238a59495055920d6a6bdfb0244ab
Rohan <rohans540@gmail.com>
2021-04-16 12:18:13 +0530

    Movie seat widget completed

--- MovieSeatBooking/script.js
+++ MovieSeatBooking/script.js
@@ -4,21 +4,52 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
+populateUI();
+
 let ticketPrice = +movieSelect.value;
+
+//Save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
 
 //Update total and count
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+    //Saving and maintaining state
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+    localStorage.setItem('selectedSeatsCount', JSON.stringify(seatsIndex));
 
     const selectedSeatsCount = selectedSeats.length;
     count.innerText = selectedSeatsCount;
     total.innerText = selectedSeatsCount * ticketPrice;
 }
 
-//Movie select event
+//Get data from localStorage and populate UI
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeatsCount'));
+    console.log(selectedSeats);
+    if(selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if(selectedSeats.indexOf(index) > -1) {
+                seat.classList.add('selected');
+            }
+        })
+    }
 
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+    if(selectedMovieIndex !== null) {
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
+
+}
+
+//Movie select event
 movieSelect.addEventListener('change', (event) => {
     ticketPrice = +event.target.value;
+    setMovieData(event.target.selectedIndex, event.target.value);
     updateSelectedCount();
 })
 
@@ -30,3 +61,5 @@ container.addEventListener('click', (event) => {
         updateSelectedCount();
     }
 })
+
+updateSelectedCount()
